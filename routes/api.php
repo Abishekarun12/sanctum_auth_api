2528cc632a2a5ed25569188bd732929338da588c
Abishek Arunachalam <abishek@cloudrevelinnovation.com>
2025-03-10 13:01:17 +0530

    init

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+
+<?php
+
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\RegisterController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('register', 'register'); //url :http://127.0.0.1:8002/api/register
+    Route::post('login', 'login');
+});
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::resource('products', ProductController::class);
+});
